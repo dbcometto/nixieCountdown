18a1d7897351741411135df3f6700cd4b4866982
dbcometto <111076949+dbcometto@users.noreply.github.com>
2025-04-16 14:19:56 -0600

    set up

---
+++ arduino_code/simpleControl/src/main.cpp
@@ -0,0 +1,11 @@
+// This project turns on and off a single Nixie tube.
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
